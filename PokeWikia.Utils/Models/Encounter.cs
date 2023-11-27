e65871b097ef4bf147c4d9aa1a379c5443cc09f6
Ian <43787552+Ian-Inizias@users.noreply.github.com>
2023-11-27 14:05:52 +0100

    Poke Wikia in webAssembly

---
+++ PokeWikia.Utils/Models/Encounter.cs
@@ -0,0 +1,34 @@
+﻿using PokeApiNet;
+
+namespace PokeWikia.Utils.Models;
+
+public enum EncounterTypes
+{
+    Land,
+    LandNight,
+    Water,
+    WaterNight,
+    OldRod,
+    GoodRod,
+    SuperRod
+}
+
+public class Encounters
+{
+    public string RouteId { get; set; }
+    public List<Encounter> WildEncounters { get; set; }
+}
+
+public class Encounter
+{
+    public int Frequency { get; set; }
+    public EncounterTypes EncounterType { get; set; }
+    public List<EncounterRatio> EncounterRatios { get; set; }
+}
+
+public class EncounterRatio
+{
+    public int Rate { get; set; }
+    public Pokemon Pokemon { get; set; }
+    public int[] Levels { get; set; }
+}
---
+++ PokeWikia.Utils/Models/PokemonTrainer.cs
@@ -0,0 +1,53 @@
+﻿using PokeApiNet;
+
+namespace PokeWikia.Utils.Models;
+
+public enum TrainerTypes
+{
+    GymLeader = 0,
+    BugcatcherF = 1,
+    BugcatcherM = 2,
+    Grunt = 3,
+    Camper = 4,
+}
+
+public class PokemonTrainer
+{
+    public string Name { get; set; }
+    public TrainerTypes TrainerType { get; set; }
+    public List<Pokemon> Team { get; set; }
+    public string Sprite { get; set; }
+
+    private static PokeApiClient _pokeApiClient = new();
+
+    public PokemonTrainer()
+    {
+    }
+
+    private PokemonTrainer(string name, TrainerTypes type, string sprite, List<Pokemon> team)
+    {
+        Name = name;
+        TrainerType = type;
+        Sprite = sprite;
+        Team = team;
+    }
+
+    /// <summary>
+    /// Builds a random trainer
+    /// </summary>
+    /// <param name="name">Name for the trainer</param>
+    /// <returns>Pokemon trainer with 6 random pokemons</returns>
+    public static async Task<PokemonTrainer> Build(string name)
+    {
+        Random rng = new();
+        var team = new List<Pokemon>();
+        var values = Enum.GetValues(typeof(TrainerTypes));
+        var type = (TrainerTypes)(values.GetValue(rng.Next(values.Length)) ?? throw new InvalidOperationException());
+        for (var i = 0; i < 6; i++)
+        {
+            team.Add(await _pokeApiClient.GetResourceAsync<Pokemon>(rng.Next(1, 1000)));
+        }
+
+        return new PokemonTrainer(name, type, team[0].Sprites.FrontDefault, team);
+    }
+}
